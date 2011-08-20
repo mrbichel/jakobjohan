fdba6fc31fbd5cfafa884f7b59408b8440e89e5e
Johan <jbl@1calendar.com>
2011-08-20 20:47:20 +0200

    Deleted local settings from git.

--- settings_local.py
+++
@@ -1,6 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.db',                      # Or path to database file if using sqlite3.
-    }
-}
