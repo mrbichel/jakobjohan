979a9a88425c6708528ede09ac7ad1e049bb991a
Johan <jbl@1calendar.com>
2011-08-21 13:53:39 +0200

    New sleeve page.

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^a/', include(admin.site.urls)),
 
     url(r'^$', 'views.home', name='home'),
-    url(r'^leather-sleeves/$', direct_to_template, {'template': 'leather-sleeves.html'}, name='leather-sleeves'),
+    url(r'^leather-sleeves/$', 'views.sleeves', name='leather-sleeves'),
     url(r'^contact/$', direct_to_template, {'template': 'contact.html'}, name='contact'),
     url(r'^story/$', direct_to_template, {'template': 'story.html'}, name='story'),
 
--- views.py
+++ views.py
@@ -7,4 +7,9 @@ def home(request):
     images = Image.objects.filter(tags__name__in=["homepage"])[:9]
 
     return render(request, 'home.html', {'images': images})
-    
+
+
+def sleeves(request):
+    images = Image.objects.filter(tags__name__in=["sleeve"])[:9]
+
+    return render(request, 'sleeves.html', {'images': images})
