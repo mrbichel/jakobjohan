6167fb904579cb34d10123c104c8265dc04c7bcf
Johan <jbl@1calendar.com>
2011-08-22 20:06:51 +0200

    Updated story.

--- urls.py
+++ urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^$', 'views.home', name='home'),
     url(r'^leather-sleeves/$', 'views.sleeves', name='leather-sleeves'),
     url(r'^contact/$', direct_to_template, {'template': 'contact.html'}, name='contact'),
-    url(r'^story/$', direct_to_template, {'template': 'story.html'}, name='story'),
+    url(r'^story/$', 'views.story', name='story'),
 
 
 )
--- views.py
+++ views.py
@@ -10,6 +10,12 @@ def home(request):
 
 
 def sleeves(request):
-    images = Image.objects.filter(tags__name__in=["sleeve"])[:9]
+    images = Image.objects.filter(tags__name__in=["sleeve"])[:3]
 
     return render(request, 'sleeves.html', {'images': images})
+
+
+def story(request):
+    images = Image.objects.filter(tags__name__in=["story"])[:3]
+
+    return render(request, 'story.html', {'images': images})
